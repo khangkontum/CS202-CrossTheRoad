7082c17c354b36b597511c74e52b50027802a6d1
Viet Nhat Thai <thaivietnhat02@gmail.com>
2021-12-10 17:15:13 +0700

    Fix bug

--- includes/Collider2D.h
+++ includes/Collider2D.h
@@ -18,16 +18,17 @@ public:
 		olc::vf2d pos;
 		olc::vf2d size;
 		olc::vf2d vel;
+
+		std::array<rect*, 4> contact;
 	};
 
-	//bool PointVsRect(const olc::vf2d& p, const rect* r);
-	//bool RectVsRect(const rect* r1, const rect* r2);
+	bool PointVsRect(const olc::vf2d& p, const rect* r);
+	bool RectVsRect(const rect* r1, const rect* r2);
 	bool RayVsRect(const olc::vf2d& ray_origin, const olc::vf2d& ray_dir, const rect* target, olc::vf2d& contact_point, olc::vf2d& contact_normal, float& t_hit_near);
 	bool DynamicRectVsRect(const rect* r_dynamic, const float fTimeStep, const rect& r_static,
 		olc::vf2d& contact_point, olc::vf2d& contact_normal, float& contact_time);
-	//bool ResolveDynamicRectVsRect(rect* r_dynamic, const float fTimeStep, rect* r_static);
+	bool ResolveDynamicRectVsRect(rect* r_dynamic, const float fTimeStep, rect* r_static);
 private:
-	std::array<rect*, 4> contact;
 	//std::vector<rect> vRects;
 };
 
