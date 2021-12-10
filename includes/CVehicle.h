b911553bba796bb32e8e1a98d0b0bb5722d4a4fe
Viet Nhat Thai <thaivietnhat02@gmail.com>
2021-12-10 17:53:01 +0700

    add getVelocity()

--- includes/CVehicle.h
+++ includes/CVehicle.h
@@ -8,11 +8,13 @@ public:
 	virtual bool isOutScreen() = 0;
 	virtual olc::vf2d size() = 0;
 	virtual olc::vf2d getPosition() = 0;
+	virtual olc::vf2d getVelocity() = 0;
 	virtual int getDirection() = 0;
 	virtual CVehicle* clone(olc::vf2d pos) = 0;
 	virtual void getName() = 0;
 protected:
 	olc::vf2d position;
+	olc::vf2d velocity;
 	olc::PixelGameEngine* pge;
 	std::shared_ptr<olc::Sprite> sprite;
 	std::shared_ptr<olc::Decal> decal;
