5bc7f371793d2a942b467fb921e451bc6302f502
viethung9at1 <viethung.9at1@gmail.com>
2021-12-16 10:49:55 +0700

    Saving data - implement

---
+++ SaveData.cpp
@@ -0,0 +1,22 @@
+#include<fstream>
+using namespace std;
+void readData(int& curentLevel, bool& themeSound, bool& gamePlaySound) {
+	ifstream fin;
+	fin.open("data.dat", ios::binary);
+	if (!fin) {
+		ofstream fout;
+		fout.open("data.dat", ios::binary);
+		curentLevel = 1;
+		themeSound = 0;
+		gamePlaySound = 0;
+		fout << curentLevel << themeSound << gamePlaySound;
+	}
+	fin >> curentLevel >> themeSound >> gamePlaySound;
+	fin.close();
+}
+void writeData(int curentLevel, bool themeSound, bool gamePlaySound) {
+	ofstream fout;
+	fout.open("data.dat", ios::binary|ios::trunc);
+	fout << curentLevel << themeSound << gamePlaySound;
+	fout.close();
+}
