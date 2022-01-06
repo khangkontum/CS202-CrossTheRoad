e72541f76b1a3e59308091763bdc9f745ff686de
viethung9at1 <viethung.9at1@gmail.com>
2022-01-06 12:25:05 +0700

    ADD: Native File Dialog

--- src/SaveData.cpp
+++ src/SaveData.cpp
@@ -34,17 +34,61 @@ void writeData(std::string configPath) {
 
 void saveGame(std::string* configPath)
 {
+	nfdchar_t* outPath = NULL;
+	nfdresult_t result = NFD_SaveDialog(NULL, NULL, &outPath);
+	if (result == NFD_OKAY)
+	{
+		writeData(outPath);
+		std::cout << "Saved\n";
+	}
+	else if (result == NFD_CANCEL) {
+		std::cout << "User pressed cancel.\n";
+	}
+	else {
+		std::cout << "Error: %s\n" << NFD_GetError() << std::endl;
+	}
+
+	/*
 	if (*configPath == "null") {
 		std::cout << "Path to save game: ";
 		std::cin >> *configPath;
 	}
 	writeData(*configPath);
 	std::cout << "Saved\n";
+	*/
 }
 
 bool loadGame(std::string* configPath)
 {
-	if (*configPath == "null") {
+	nfdchar_t* outPath = NULL;
+	nfdresult_t result = NFD_OpenDialog(NULL, NULL, &outPath);
+
+	if (result == NFD_OKAY) {
+
+		std::cout << "Path : " << outPath << std::endl;
+
+		if (readData(outPath))
+		{
+			std::cout << "Loaded\n";
+			return true;
+		}
+		else
+		{
+			std::cout << "No such file or directory" << std::endl;
+			*configPath = "null";
+			return false;
+		}
+		free(outPath);
+	}
+	else if (result == NFD_CANCEL) {
+		std::cout << "User pressed cancel.\n";
+	}
+	else {
+		std::cout << "Error: %s\n" << NFD_GetError() << std::endl;
+	}
+	return false;
+
+	/*if (*configPath == "null") {
 		std::cout << "Path to load game: ";
 		std::cin >> *configPath;
 	}
@@ -58,5 +102,5 @@ bool loadGame(std::string* configPath)
 		std::cout << "No such file or directory" << std::endl;
 		*configPath = "null";
 		return false;
-	}	
+	}*/
 }
